7006550ffe1bbf2b658599f98a8a6bdf421a2867
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2024-07-24 10:06:20 -0300

    create trip controller and route

---
+++ src/controllers/trip_creator.py
@@ -0,0 +1,34 @@
+import uuid
+from src.models.repositories.trips_repository import TripsRepository
+from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
+
+class TripCreator():
+    def __init__(self, trips_repository: TripsRepository, emails_repository: EmailsToInviteRepository) -> None:
+        self.__trip_repository = trips_repository
+        self.__emails_repository: emails_repository
+
+    def create(self, body) -> dict:
+        try:
+            trip_id = str(uuid.uuid4())
+            trip_infos = { **body, "id": trip_id }
+            self.__trip_repository.create_trip(trip_infos)
+
+            emails = body.get("emails_to_invite")
+            if emails:
+                for email in emails:
+                    self.__emails_repository.registry_email({
+                        "id": str(uuid.uuid4()),
+                        "trip_id": trip_id,
+                        "email": email
+                    })
+            
+            return {
+                "body": { "id": trip_id },
+                "status_code": 201
+            }
+        
+        except Exception as excepetion:    
+            return {
+                "body": { "error": "Bad Request", "message": str(excepetion) },
+                "status_code": 400
+            }
